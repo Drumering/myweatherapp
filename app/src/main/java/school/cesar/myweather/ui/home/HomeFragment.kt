db3aea5b0bfcf8720faae2b317bdadece2aa542b
Anderson Mendes de Almeida <ama@cesar.school>
2021-06-05 17:35:52 -0300

    [TASK]Created the project

---
+++ app/src/main/java/school/cesar/myweather/ui/home/HomeFragment.kt
@@ -0,0 +1,78 @@
+package school.cesar.myweather.ui.home
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import school.cesar.myweather.R
+import school.cesar.myweather.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+
+        binding.searchBarHome.setEndIconOnClickListener {
+
+            if (context != null) {
+                if (!isInternetAvailable(requireContext())){
+                    Toast.makeText(context, "No internet connection", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        return root
+    }
+
+    private fun isInternetAvailable(context: Context): Boolean {
+        var result = false
+
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+
+        cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+            result = when {
+                hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        }
+
+        return result
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/school/cesar/myweather/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,73 @@
+package school.cesar.myweather.ui.notifications
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import school.cesar.myweather.R
+import school.cesar.myweather.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: NotificationsViewModel
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textNotifications
+//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+        val sharedPreferences = context?.getSharedPreferences(getString(R.string.pref_file), Context.MODE_PRIVATE)
+
+        sharedPreferences?.let {
+            val langPT = it.getBoolean("lang_PT", false)
+            val tempC = it.getBoolean("temp_C", false)
+
+            if (langPT) {
+                binding.rbPtBr.isChecked = true
+            } else {
+                binding.rbEnUs.isChecked = true
+            }
+
+            if (tempC) {
+                binding.rbCelsius.isChecked= true
+            } else {
+                binding.rbFarenheit.isChecked = true
+            }
+        }
+
+        binding.btnSave.setOnClickListener {
+            with(sharedPreferences?.edit()) {
+                this?.putBoolean("lang_PT", binding.rbPtBr.isChecked)
+                this?.putBoolean("temp_C", binding.rbCelsius.isChecked)
+                this?.apply()
+            }
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
